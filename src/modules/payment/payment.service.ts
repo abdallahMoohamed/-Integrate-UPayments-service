263d3aca8dd804d137103373d486f8a8c0860f86
abdallahMoohamed <abdullamoohammed@gmail.com>
2024-06-04 17:30:27 +0300

    finish task in payment method

--- src/modules/payment/payment.service.ts
+++ src/modules/payment/payment.service.ts
@@ -2,6 +2,8 @@ import { HttpService } from "@nestjs/axios";
 import { HttpException, Injectable } from "@nestjs/common";
 import { AxiosResponse } from "axios";
 import { lastValueFrom } from "rxjs";
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 @Injectable()
 export class PaymentService {
@@ -9,49 +11,33 @@ export class PaymentService {
 
   async makeCharge (data: any, authorization: any): Promise<any> {
     try {
-      const DATA = {
-        products: [
-          {
-            "name": "Logitech K380",
-            "description": "Logitech K380 / Easy-Switch for Upto 3 Devices, Slim Bluetooth Tablet Keyboar ",
-            "price": 10,
-            "quantity": 1
-          },
-          {
-            "name": "Logitech K380",
-            "description": "Logitech K380 / Easy-Switch for Upto 3 Devices, Slim Bluetooth Tablet Keyboar ",
-            "price": 10,
-            "quantity": 1
-          }],
+      // prepare data object before send to Upayments url
+      const paymentData = {
+        products: data.order.products,
         order: {
-          "id": "202210101255255144669", // mandatory
-          "description": "Purchase order received for Logitech K380 Keyboard", //mandatory
-          "currency": "KWD", //mandatory
-          "amount": 20 //mandatory
+          id: data.order._id, // mandatory
+          description: data.order.description, //mandatory
+          currency: "KWD", //mandatory
+          amount: data.order.price //mandatory I clac that 
         },
         paymentGateway: { src: "knet" }, // knet, cc, samsung-pay, apple-pay, google-pay and create-invoice
-        language: "en",
+        language: "en",// mandatory
         reference: {
-          id: "Refers to the order or transaction ID in your own system we will save your ID for our reference."
+          id: data.order._id //"Refers to the order or transaction ID in your own system we will save your ID for our reference."
         },
-        customer: {
-          "uniqueId": "2129879kjbljg767881",
-          "name": "Dharmendra Kakde",
-          "email": "kakde.dharmendra@upayments.com",
-          "mobile": "+96566336537"
-        },
-        returnUrl: "https://upayments.com/en/",
-        cancelUrl: "https://error.com",
-        notificationUrl: "https://webhook.site/d7c6e1c8-b98b-4f77-8b51-b487540df336",
-        customerExtraData: "User define data"
+        returnUrl: "http://localhost:3000/order/successfully-transaction",//mandatory: url for successful payment
+        cancelUrl: "https://error.com", //mandatory: url for cancel or failure payment
+        notificationUrl: "https://webhook.site/d7c6e1c8-b98b-4f77-8b51-b487540df336",//mandatory:  will receive the webhook data
       }
-      const url = 'https://sandboxapi.upayments.com/api/v1/charge'
 
-      const response: AxiosResponse<any> = await lastValueFrom(this.httpService.post(url, data, { headers: { authorization } }));
+      // user api for make charge by UPayments service 
+      const url = process.env.URL_CHARGE
+      const response: AxiosResponse<any> = await lastValueFrom(this.httpService.post(url, paymentData, { headers: { authorization } }));
 
-      // check is transcation done or not 
+      // return response  
       return response.data;
-    } catch (error) {    
+
+    } catch (error) {
       throw new HttpException(error.message, error.response.status);
     }
   }
